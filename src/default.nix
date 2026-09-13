4bdee770ca74beeff8a7dad2b95a557e35d03d10
zudww <zudwduz@gmail.com>
2026-09-13 20:05:05 +0800

    feat: import functions to `lib` toplevel

--- src/default.nix
+++ src/default.nix
@@ -1,10 +1,31 @@
 { nixlib-general, ... }:
-let lib = nixlib-general.lib; in
 {
-  lib =
-    {
-      mixins = import ./mixins.nix lib;
-      process = import ./process.nix lib;
-      steps = import ./steps.nix lib;
-    };
+  lib = let
+    inputs = nixlib-general.lib;
+
+    mixins = import ./mixins.nix inputs;
+    process = import ./process.nix inputs;
+    steps = import ./steps.nix inputs;
+  in {
+    inherit mixins process steps;
+
+    inherit (mixins)
+      insertAfterFirstId
+      insertAfterId
+      insertAfterIndex
+      insertBeforeFirstId
+      insertBeforeId
+      insertBeforeIndex
+      replaceAtIndex
+      replaceId
+      removeAtIndex
+      removeId
+      ;
+
+    inherit (process)
+      runProcess;
+
+    inherit (steps)
+      mkStep;
+  };
 }
